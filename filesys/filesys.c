28a5a78d1d35bf17b57a475eb1bcca07832c9d69
sh940701 <visioner2168@gmail.com>
2024-05-23 20:52:43 +0000

    formatter

--- filesys/filesys.c
+++ filesys/filesys.c
@@ -7,6 +7,7 @@
 #include "filesys/inode.h"
 #include "filesys/directory.h"
 #include "devices/disk.h"
+#include "filesys/fat.h"
 
 /* The disk that contains the file system. */
 struct disk *filesys_disk;
@@ -61,10 +62,10 @@ filesys_done (void) {
  * or if internal memory allocation fails. */
 bool
 filesys_create (const char *name, off_t initial_size) {
-	disk_sector_t inode_sector = 0;
+	disk_sector_t inode_sector = find_empty_fat();
 	struct dir *dir = dir_open_root ();
 	bool success = (dir != NULL
-			&& free_map_allocate (1, &inode_sector)
+			// && free_map_allocate (1, &inode_sector)
 			&& inode_create (inode_sector, initial_size)
 			&& dir_add (dir, name, inode_sector));
 	if (!success && inode_sector != 0)
